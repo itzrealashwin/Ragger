c8d2f805566df29ec036e564bcb12e1f51b82c2f
Ashwin <maliashwin2005@gmail.com>
2025-08-20 16:55:21 +0530

    CSS fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,22 +15,27 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div className="flex flex-col justify-center min-h-screen bg-white text-black dark:bg-black dark:text-white">
-              <Header />
-              <main className="flex-grow">
-                {children}
-              </main>
-            </div>
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+        <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+              <link href="https://fonts.googleapis.com/css2?family=Qwitcher+Grypen:wght@400;700&display=swap" rel="stylesheet"></link>
+            </head>
+            <body className={inter.className}>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem
+                disableTransitionOnChange
+              >
+                <div className="flex flex-col justify-center min-h-screen bg-white text-black dark:bg-black dark:text-white">
+                  <Header />
+                  <main className="flex-grow">
+                    {children}
+                  </main>
+                </div>
+              </ThemeProvider>
+            </body>
+          </html>
+        </ClerkProvider>
+        );
 }
